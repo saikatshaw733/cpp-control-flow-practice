8fe309f4325f5fccc461f59eb2ab8e097eaaaee5
saikatshaw733 <saikatshaw733@gmail.com>
2026-06-11 14:55:44 +0530

    Update array.cpp
    Added a new function to find whether an element is in an array

--- array.cpp
+++ array.cpp
@@ -8,10 +8,30 @@ double gettotal(double prices[],int size)
 }
 return total;
 }
+double find(double prices[],int size)
+{
+    
+    double x;
+    std::cout<<"\nwhat is the number you want to find? ";
+    std::cin>>x;
+    for(int i=0; i<size;i++)
+    {
+        if(x == prices[i])
+        {
+            std::cout<<"\nthe number "<<x<<" is located in "<<i+1<<" position\n";
+            break;}
+        else std::cout<<"not in the array\n";
+        break;
+    }
+
+}
+
 int main() {
     double prices[]={1,2,3,4,5,6};
     int size = sizeof(prices)/sizeof(double);
     double total = gettotal(prices,size);
     std::cout<<"the total price is "<<total;
+    double find_num = find(prices,size);
+
     return(0);
 }
